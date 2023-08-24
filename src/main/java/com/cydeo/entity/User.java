fdb4fbfe646a4cd7999834c01c2a2dc2a2724711
efecagrisen <efecagrisen@yahoo.com>
2023-08-24 11:01:51 +0300

    started building User Create Page

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,17 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+
+public class User extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String passWord;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+
+
+}
