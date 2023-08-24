a4df50cc976dbfbe5f4b5ccd1dc1a58ef29d62aa
efecagrisen <efecagrisen@yahoo.com>
2023-08-24 16:31:20 +0300

    entity and DTO packages added

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,7 +1,14 @@
 package com.cydeo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity {
 
     private Long id;
@@ -9,5 +16,4 @@ public class BaseEntity {
     private Long insertUserId;
     private LocalDateTime lastUpdateDateTime;
     private Long lastUpdateUserId;
-
 }
