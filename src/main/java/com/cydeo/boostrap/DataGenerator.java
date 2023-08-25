510a2826177d375c081397c4ba02d10c74ee93d7
efecagrisen <efecagrisen@yahoo.com>
2023-08-25 07:07:24 +0300

    roles are reflected to browser

--- src/main/java/com/cydeo/boostrap/DataGenerator.java
+++ src/main/java/com/cydeo/boostrap/DataGenerator.java
@@ -4,6 +4,7 @@ import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
 import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +12,11 @@ import org.springframework.stereotype.Component;
 public class DataGenerator implements CommandLineRunner {
 
     private final RoleService roleService;
+    private final UserService userService;
 
-    public DataGenerator(RoleService roleService) {
+    public DataGenerator(RoleService roleService, UserService userService) {
         this.roleService = roleService;
+        this.userService = userService;
     }
 
     @Override
@@ -29,6 +32,7 @@ public class DataGenerator implements CommandLineRunner {
         roleService.save(managerRole);
         roleService.save(employeeRole);
 
+        //create users and put in the DB(map)
         UserDTO user1 = new UserDTO("John", "Kesy",
                 "john@cydeo.com", "Abc1", true, "7459684532", managerRole, Gender.MALE);
         UserDTO user5 = new UserDTO("Mike", "Smith",
@@ -39,15 +43,24 @@ public class DataGenerator implements CommandLineRunner {
                 "craig@cydeo.com", "Abc3", true, "7777775566", employeeRole, Gender.MALE);
         UserDTO user4 = new UserDTO("Shaun",
                 "Hayns", "shaun@cydeo.com", "Abc4", true, "3256987412", managerRole, Gender.MALE);
-        UserDTO user6 = new UserDTO("Elizebeth",
+        UserDTO user6 = new UserDTO("Elizabeth",
                 "Loren", "elizebeth@cydeo.com", "Abc4", true, "5306987412", employeeRole, Gender.FEMALE);
         UserDTO user7 = new UserDTO("Maria",
                 "Ada", "maria@cydeo.com", "Abc4", true, "9996987412", employeeRole, Gender.FEMALE);
         UserDTO user8 = new UserDTO("Bill",
                 "Matt", "bill@cydeo.com", "Abc4", true, "8881239846", employeeRole, Gender.MALE);
 
+            userService.save(user1);
+            userService.save(user2);
+            userService.save(user3);
+            userService.save(user4);
+            userService.save(user5);
+            userService.save(user6);
+            userService.save(user7);
+            userService.save(user8);
 
-        //create users and put in the DB(map)
+
+
 
 
     }
